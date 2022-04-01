5633b15adbab0703436652efb83997d6412200fd
Tuisku <tumipo@utu.fi>
2022-04-01 16:00:58 +0300

    control buttons in the browser

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Gamepad from 'react-gamepad'
 import {Component, React} from 'react'
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import env from "react-dotenv";
-import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, ButtonGroup } from 'react-bootstrap';
 
-const client = new W3CWebSocket(env.API_URL);
+const client = new W3CWebSocket(env.PETTERI);
 
 client.onopen = () => {
     console.log('WebSocket Client Connected');
-    document.querySelector('#server-info').textContent = 'Connected to ' + env.API_URL;
+    document.querySelector('#server-info').textContent = env.PETTERI;
 };
 
 client.onmessage = (message) => {
@@ -24,11 +24,12 @@ client.onerror = function() {
     document.querySelector('#server-info').textContent = 'Connection Error';
 };
 
-function handleControllerInput(type, buttonName, value) {
-  console.log("Sending to server:", type, buttonName, value);
+function sendControlMessage(type, identifier, value) {
+  console.log("Sending to server:", type, identifier, value);
+  document.querySelector('#client-message').textContent = "type: "+type+"\nid: "+identifier+"\nvalue: " + value;
   client.send(JSON.stringify({
     type: type,
-    button: buttonName,
+    id: identifier,
     value: value
   }));
 }
@@ -46,7 +47,7 @@ class GameControl extends Component {
   }
  
   buttonChangeHandler(buttonName, down) {
-    handleControllerInput(
+    sendControlMessage(
       "buttonChange",
       buttonName,
       down ? 0 : 1);
@@ -55,27 +56,22 @@ class GameControl extends Component {
   axisChangeHandler(axisName, value, previousValue) {
     console.log(axisName, value)
     document.querySelector('#button-info').textContent = ('Latest input: ' + axisName + ' ' + value);
-    handleControllerInput(
-      "axisChange",
-      axisName,
-      value);
+    if (axisName == 'LeftStickY'){
+      axisMoveY(value);
+    }
+    //sendControlMessage(
+    //  "axisChange",
+    //  axisName,
+    //  value);
   }
  
   buttonDownHandler(buttonName) {
     console.log(buttonName, 'down');
-    //client.send(JSON.stringify({
-    //  type: "contentchange",
-    //  content: "buttonDown"
-    //}));
   }
  
   buttonUpHandler(buttonName) {
     console.log(buttonName, 'up')
     document.querySelector('#button-info').textContent = ('Latest input: ' + buttonName);
-    //client.send(JSON.stringify({
-    //  type: "contentchange",
-    //  content: "buttonUp"
-    //}));
   }
  
   render() {
@@ -98,20 +94,40 @@ class GameControl extends Component {
   }
 }
 
-function openGripper(){
-  console.log("open gripper");
-  client.send(JSON.stringify({
-    type: "gripperCommand",
-    command: "open"
-  }));
+function operateGripper(operation){
+  // 1 = open gripper
+  // 0 = close gripper
+  // for now
+  if (operation){
+    console.log("open gripper");
+  }
+  else {
+    console.log("close gripper");
+  }
+  sendControlMessage(
+    "action",
+    "gripper",
+    operation
+  );
 }
 
-function closeGripper(){
-  console.log("close gripper");
-  client.send(JSON.stringify({
-    type: "gripperCommand",
-    command: "close"
-  }));
+function axisMoveX(value){
+  sendControlMessage(
+    "axisChange",
+    "X",
+    value);
+}
+function axisMoveY(value){
+  sendControlMessage(
+    "axisChange",
+    "Y",
+    value);
+}
+function axisMoveZ(value){
+  sendControlMessage(
+    "axisChange",
+    "Z",
+    value);
 }
 
 class ServerStatus extends Component {
@@ -122,6 +138,8 @@ class ServerStatus extends Component {
         <Card.Body>
           <Card.Title>Connection</Card.Title>
           <Card.Text id="server-info">Not connected</Card.Text>
+          <Card.Title>Sent message</Card.Title>
+          <Card.Text id="client-message">No messages sent.</Card.Text>
           <Card.Title>Messages from server</Card.Title>
           <Card.Text id="server-message">No messages from server</Card.Text>
         </Card.Body>
@@ -130,15 +148,47 @@ class ServerStatus extends Component {
   }
 }
 
+class ControlButtons extends Component {
+    render() {
+    return (
+      <Container id="gripperButtons">
+        <Col>
+          <Row>Gripper control</Row>    
+          <ButtonGroup className="me-2" aria-label="Gripper buttons">
+            <Button variant="primary" onClick={ () => operateGripper(1) }>Open gripper</Button>
+            <Button variant="primary" onClick={ () => operateGripper(0) }>Close gripper</Button>
+          </ButtonGroup> 
+          <Row>Arm movement</Row>    
+          
+          <ButtonGroup vertical className="me-2" aria-label="Z axis buttons">
+            <Button  variant="info" onClick={ () => axisMoveZ(-0.2) }>Up</Button>
+            <Button variant="secondary" onClick={ () => axisMoveZ(0) }>-</Button>
+            <Button variant="info" onClick={ () => axisMoveZ(0.2) }>Down</Button>
+          </ButtonGroup>
+
+          <ButtonGroup vertical className="me-2" aria-label="X axis buttons">
+            <Button  variant="info" onClick={ () => axisMoveX(0.2) }>Front</Button>
+            <Button variant="secondary" onClick={ () => axisMoveX(0) }>-</Button>
+            <Button variant="info" onClick={ () => axisMoveX(-0.2) }>Back</Button>
+          </ButtonGroup>
+
+          <ButtonGroup className="me-2" aria-label="Y axis buttons">
+            <Button  variant="info" onClick={ () => axisMoveY(-0.2) }>Left</Button>
+            <Button variant="secondary" onClick={ () => axisMoveY(0) }>-</Button>
+            <Button variant="info" onClick={ () => axisMoveY(0.2) }>Right</Button>
+          </ButtonGroup>
+
+        </Col>
+      </Container>
+    )
+  }
+}
+
 class ControllerStatus extends Component {
   render() {
     return (
     <div className="App-header">
-      <p><img src={logo} className="App-logo" alt="logo" /></p>
-      <div id="gripperButtons">
-        <button onClick={openGripper}>Open gripper</button>
-        <button onClick={closeGripper}>Close gripper</button>
-      </div>
+        <ControlButtons/>
         <GameControl/>
       <div id="round-button"></div>
       <p id="button-info"></p>
@@ -152,12 +202,19 @@ function App() {
     <div className="App">
       <Container fluid className="Container">
         <Row>
+
           <Col id="server-col" className="d-flex justify-content-center align-items-center">
             <ServerStatus />
           </Col>
+
           <Col id="client-col" className="d-flex justify-content-center align-items-center text-center">
             <ControllerStatus />
           </Col>
+
+          <Col id="server-col" className="d-flex justify-content-center align-items-center">  
+           <p><img src={logo} className="App-logo" alt="logo" /></p>
+          </Col>
+
         </Row>
       </Container>
     </div>
